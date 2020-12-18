94eaffa8c69c58b425463d9cf3a95c45ff63c7bb
Matthias Nieuwenhuisen <matthias@nieuwenhuisen.de>
2020-12-18 19:19:18 +0100

    use other data source

--- covid19_incidence.py
+++ covid19_incidence.py
@@ -19,9 +19,15 @@ def exponential_fit(x, a, b, c):
 
 
 #dataset = pandas.read_csv("~/Downloads/v3Nbr69N",sep=",",header=0,encoding="utf8",index_col=0,parse_dates=True,dayfirst=True)
-dataset_dl = pandas.read_csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv",sep=",",header=0,encoding="utf8",index_col=0,parse_dates=True,dayfirst=True)
+#dataset_dl = pandas.read_csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv",sep=",",header=0,encoding="utf8",index_col=0,parse_dates=True,dayfirst=True)
 dataset_bonn_dl = pandas.read_csv("https://stadtplan.bonn.de/csv?OD=4379",sep=";",header=0,encoding="utf8",index_col=0,parse_dates=True)
 dataset_muenster_dl = pandas.read_csv("https://raw.githubusercontent.com/od-ms/resources/master/coronavirus-fallzahlen-regierungsbezirk-muenster.csv",sep=",",header=0,encoding="utf8",parse_dates=True,dayfirst=True)
+
+# Select either offical data with Meldeverzug or crowd sourced data
+#dataset_states_dl = pandas.read_csv("https://raw.githubusercontent.com/jgehrcke/covid-19-germany-gae/master/cases-rki-by-state.csv",sep=",",header=0,encoding="utf8",index_col=0,parse_dates=True)
+dataset_states_dl = pandas.read_csv("https://raw.githubusercontent.com/jgehrcke/covid-19-germany-gae/master/cases-rl-crowdsource-by-state.csv",sep=",",header=0,encoding="utf8",index_col=0,parse_dates=True)
+
+states_ew_dl = pandas.read_csv("https://www.nieuwenhuisen.de/states_ew.csv",sep=",",header=0,encoding="utf8",index_col=0)
 
 
 # In[ ]:
@@ -29,28 +35,43 @@ dataset_muenster_dl = pandas.read_csv("https://raw.githubusercontent.com/od-ms/r
 
 # Setup
 #countries = ["DE","SE","US"]
-countries = ["DE","BONN","MUENSTER"]
+countries = ["BN","DE","DE-NW","DE-SN"]
 max_days=60
-extrapolation_look_back = 20
-extrapolation_future = 5
+extrapolation_look_back = 10
+extrapolation_future = 4
 
 dataset = dataset_dl
+dataset_states = dataset_states_dl
 dataset_bonn = dataset_bonn_dl
 dataset_regbez_muenster = dataset_muenster_dl
+states_ew = states_ew_dl.to_dict(orient="index")
 
 
 # In[ ]:
 
 
-if "BONN" in countries:
-    # Convert Bonn dataset to ECDC format and append
-    diff_bonn = dataset_bonn.sort_index(ascending=False).diff(periods=-1)
-    diff_bonn['popData2019'] = 330224
-    diff_bonn['countriesAndTerritories'] = 'Bonn'
-    diff_bonn['geoId'] = 'BONN'
-    diff_bonn = diff_bonn.rename(columns={"positiv_getest": "cases"})
-    relevant_bonn = diff_bonn[['geoId','cases','popData2019','countriesAndTerritories']]
-    dataset = dataset.append(relevant_bonn)
+# Convert Bonn dataset to ECDC format and append
+dataset_bonn_sorted = dataset_bonn.sort_index(ascending=True)
+dataset_bonn_sorted.index = dataset_bonn_sorted.index.shift(-1, freq='D')
+dataset_bonn_sorted = dataset_bonn_sorted.rename(columns={"positiv_getest": "BN"})
+relevant_bonn = dataset_bonn_sorted[['BN']]
+
+dataset_states = dataset_states.rename(columns={"sum_cases": "DE"})
+dataset_states.index = dataset_states.index.date
+merged_data = pandas.merge(relevant_bonn,dataset_states,how='left',left_index=True,right_index=True)
+merged_data_diff = merged_data.diff()
+merged_data_diff = merged_data_diff.sort_index(ascending=False)
+
+
+# In[ ]:
+
+
+for country in countries:
+    merged_data_diff["cases_per_100k_"+country] = merged_data_diff[country] * 100000 / states_ew[country]['Ew']
+
+    indexer = pandas.api.indexers.FixedForwardWindowIndexer(window_size=7)
+for country in countries:
+    merged_data_diff['cum_cases_7_days_'+country] = merged_data_diff['cases_per_100k_'+country].shift(1).rolling(window=indexer).sum()
 
 
 # In[ ]:
@@ -77,30 +98,30 @@ if "MUENSTER" in countries:
 
 
 # Get relevant columns
-relevant = dataset[['geoId','cases','popData2019','countriesAndTerritories']]
+#relevant = dataset[['geoId','cases','popData2019','countriesAndTerritories']]
 # Put requested countries in different tables, rename columns with country suffix for later merging
 
-countrydata = {}
-for country in countries:
-    countrydata[country] = relevant[relevant['geoId'].isin([country])]
-    countrydata[country] = countrydata[country].rename(columns={"cases": "cases_"+country, "popData2019": "popData2019_"+country})
+#countrydata = {}
+#for country in countries:
+#    countrydata[country] = relevant[relevant['geoId'].isin([country])]
+#    countrydata[country] = countrydata[country].rename(columns={"cases": "cases_"+country, "popData2019": "popData2019_"+country})
 
 # Merge country data into single table
-merged_data = countrydata[countries[0]]
-for country in countries[1:]:
-    merged_data = pandas.merge(merged_data,countrydata[country],how='left',left_index=True,right_index=True)
+#merged_data = countrydata[countries[0]]
+#for country in countries[1:]:
+#    merged_data = pandas.merge(merged_data,countrydata[country],how='left',left_index=True,right_index=True)
 
 
 # In[ ]:
 
 
 # Calculate 7-day incidence
-for country in countries:
-    merged_data["cases_per_100k_"+country] = merged_data["cases_"+country] * 100000 / merged_data["popData2019_"+country]
+#for country in countries:
+#    merged_data["cases_per_100k_"+country] = merged_data["cases_"+country] * 100000 / merged_data["popData2019_"+country]
 
-indexer = pandas.api.indexers.FixedForwardWindowIndexer(window_size=7)
-for country in countries:
-    merged_data['cum_cases_7_days_'+country] = merged_data['cases_per_100k_'+country].shift(1).rolling(window=indexer).sum()
+#indexer = pandas.api.indexers.FixedForwardWindowIndexer(window_size=7)
+#for country in countries:
+#    merged_data['cum_cases_7_days_'+country] = merged_data['cases_per_100k_'+country].shift(1).rolling(window=indexer).sum()
 
 pyplot.rcParams.update({'font.size': 22})
 # Plot data
@@ -108,14 +129,17 @@ fig = pyplot.figure(figsize=(16, 12))
 ax1 = fig.add_subplot(1,1,1)
 
 ax1.set_ylabel('Neuinfektionen pro 7 Tage pro 100.000 Einwohner')
+ax1.tick_params(labelright=True)
+
 for country in countries:
     color = next(ax1._get_lines.prop_cycler)['color']
-    country_name = dataset[dataset['geoId']==country].head(1)['countriesAndTerritories'].iloc[0]
-    merged_data[1:max_days]['cum_cases_7_days_'+country].plot(kind='line',ax=ax1,label=country_name,color=color,linewidth=3)
+    #country_name = dataset[dataset['geoId']==country].head(1)['countriesAndTerritories'].iloc[0]
+    country_name = states_ew[country]['State']
+    merged_data_diff[1:max_days]['cum_cases_7_days_'+country].plot(kind='line',ax=ax1,label=country_name,color=color,linewidth=3)
     # extrapolation
     try:
         if country != "MUENSTER":
-            y = merged_data[1:extrapolation_look_back]['cum_cases_7_days_' + country]
+            y = merged_data_diff[1:extrapolation_look_back]['cum_cases_7_days_' + country]
             i = y.index.to_pydatetime()
             xi = mdates.date2num(i)
             p0 = [1,1,y[-1]]
@@ -139,21 +163,23 @@ pyplot.text('2020-11-03', # x-value from data
                  80, # wiggle the labels 2so they don't overlap
                  'Lockdown Light', # string label
                  color='black',rotation=90)
-#pyplot.axvline(x='2020-12-24', ymin=0, ymax=160, linestyle='--',color='black',label="Heilig Abend")
-#pyplot.text('2020-12-25', # x-value from data
-#                 80, # wiggle the labels 2so they don't overlap
-#                 'Heilig Abend', # string label
-#                 color='black',rotation=90)
+pyplot.axvline(x='2020-12-24', ymin=0, ymax=160, linestyle='--',color='black',label="Heilig Abend")
+pyplot.text('2020-12-25', # x-value from data
+                 80, # wiggle the labels 2so they don't overlap
+                 'Heilig Abend', # string label
+                 color='black',rotation=90)
 
 pyplot.axvline(x='2020-12-02', ymin=0, ymax=160, linestyle='--',color='black',label="Heilig Abend")
 
+pyplot.savefig("/home/nieuwenh/ownCloud/cases.png")
 pyplot.show()
 
 # Print last two days
-print( merged_data[1:2].index.date[0] )
+print( merged_data_diff[0:1].index.date[0] )
 for country in countries:
-    country_name = dataset[dataset['geoId']==country].head(1)['countriesAndTerritories'].iloc[0]
-    country_incidence = merged_data[1:2]['cum_cases_7_days_'+country].iloc[0]
-    country_incidence_yesterday = merged_data[2:3]['cum_cases_7_days_'+country].iloc[0]
+    #country_name = dataset[dataset['geoId']==country].head(1)['countriesAndTerritories'].iloc[0]
+    country_name = states_ew[country]['State']
+    country_incidence = merged_data_diff[1:2]['cum_cases_7_days_'+country].iloc[0]
+    country_incidence_yesterday = merged_data_diff[2:3]['cum_cases_7_days_'+country].iloc[0]
     print( "{}: {:.1f} (Gestern: {:.1f})".format( country_name, country_incidence, country_incidence_yesterday ) )
 
